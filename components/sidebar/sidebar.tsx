e12ab40d549ba15ecd3f6a29b031f25e3f940174
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2023-06-22 16:19:55 +0100

    fix

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -29,56 +29,52 @@ export const SidebarWrapper = () => {
   if (router.pathname === "/") {
     return null;
   }
-         <Sidebar collapsed={collapsed}>
-            <Sidebar.Header>
-               <CompaniesDropdown />
-            </Sidebar.Header>
-            <Flex
-               direction={'column'}
-               justify={'between'}
-               css={{height: '100%'}}
-            >
-               <Sidebar.Body className="body sidebar">
-                  <SidebarItem
-                     title="Home"
-                     icon={<HomeIcon />}
-                     isActive={router.pathname === '/'}
-                     href="/"
-                  />
-                  <SidebarMenu title="Main Menu">
-                     <SidebarItem
-                        isActive={router.pathname === '/accounts'}
-                        title="Accounts"
-                        icon={<AccountsIcon />}
-                        href="accounts"
-                     />
-                     <SidebarItem
-                        isActive={router.pathname === '/products'}
-                        title="Products"
-                        icon={<ProductsIcon />}
-                        href="products"
-                     />
-                    
-                  </SidebarMenu>
 
-                  
-               </Sidebar.Body>
-               <Sidebar.Footer>
-                  <Tooltip content={'Settings'} rounded color="primary">
-                     <SettingsIcon />
-                  </Tooltip>
-                  <Tooltip content={'Adjustments'} rounded color="primary">
-                     <FilterIcon />
-                  </Tooltip>
-                  <Tooltip content={'Profile'} rounded color="primary">
-                     <Avatar
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                        size={'sm'}
-                     />
-                  </Tooltip>
-               </Sidebar.Footer>
-            </Flex>
-         </Sidebar>
-      </Box>
-   );
+  return (
+    <Box>
+      <Sidebar collapsed={collapsed}>
+        <Sidebar.Header>
+          <CompaniesDropdown />
+        </Sidebar.Header>
+        <Flex direction="column" justify="between" css={{ height: '100%' }}>
+          <Sidebar.Body className="body sidebar">
+            <SidebarItem
+              title="Home"
+              icon={<HomeIcon />}
+              isActive={router.pathname === '/'}
+              href="/"
+            />
+            <SidebarMenu title="Main Menu">
+              <SidebarItem
+                isActive={router.pathname === '/accounts'}
+                title="Accounts"
+                icon={<AccountsIcon />}
+                href="/accounts"
+              />
+              <SidebarItem
+                isActive={router.pathname === '/products'}
+                title="Products"
+                icon={<ProductsIcon />}
+                href="/products"
+              />
+            </SidebarMenu>
+          </Sidebar.Body>
+          <Sidebar.Footer>
+            <Tooltip content={'Settings'} rounded color="primary">
+              <SettingsIcon />
+            </Tooltip>
+            <Tooltip content={'Adjustments'} rounded color="primary">
+              <FilterIcon />
+            </Tooltip>
+            <Tooltip content={'Profile'} rounded color="primary">
+              <Avatar
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                size={'sm'}
+              />
+            </Tooltip>
+          </Sidebar.Footer>
+        </Flex>
+      </Sidebar>
+    </Box>
+  );
 };
